d62988dd32479114d04ae7358c6822eb1eff90d8
Pesrine <482566032@qq.com>
2024-09-26 00:07:41 +0800

    new

---
+++ src/3d_bbs/README.md
@@ -0,0 +1,130 @@
+<div align="center">
+  <h1>3D-BBS: Global Localization for 3D Point Cloud Scan Matching Using Branch-and-Bound Algorithm</h1>
+  <br>
+  <p>This repository contains the public source code for the paper to be published at ICRA2024 in Yokohama.</p>
+  <p>If you like the 3D-BBS, we kindly ask for your support by giving our repository star ⭐!</p>
+  <p><a href="https://arxiv.org/abs/2310.10023"><b>📄  Paper</b></a> | <a href="https://youtu.be/QGxnDnwkG1k"><b>🎞️ Video</b></a></p>
+</div>
+
+```
+@inproceedings{aoki20243dbbs,
+  title={3D-BBS: Global Localization for 3D Point Cloud Scan Matching Using Branch-and-Bound Algorithm}, 
+  author={Koki Aoki and Kenji Koide and Shuji Oishi and Masashi Yokozuka and Atsuhiko Banno and Junichi Meguro},
+  booktitle={IEEE International Conference on Robotics and Automation},
+  year={2024},
+  organization={IEEE}
+}
+```
+
+## 🔍 Quick understanding about 3D-BBS
+<img alt="overview" src="figs/overview.jpg" width="50%">
+
+### Task
+- 3D global localization for such as robot's initial pose estimation
+
+### Input
+  - A single 3D LiDAR scan aligned in the gravity direction by using such as IMU   
+  (Although 3D-BBS also performs 6DoF search without gravity aligning, the processing time is more than 10 times longer.)
+  - 3D pre-built point cloud map
+
+### Output
+- $4\times4$ transformation matrix about coarse sensor pose in map coordinates.
+
+### Main Features 
+- **Full search algorithm** based on branch-and-bound (BnB) scan matching
+- **Fast processing (latest: about 20 ms ~)** by proposed GPU-accelerated BnB and roto-translational branching
+- **No initial pose** (we assume that the roll and pitch angles are roughly estimated by IMU for faster processing)
+- **Low voxelmap memory consumption** due to sparse hash map
+
+### Other Features
+<details><summary> Click here! </summary><div>
+
+- Optional **Voxelmap pre-construction** and direct read function
+- Optional **timeout** function
+- **Minimal dependencies** in core source code so that user can **easily integrate** into the other projects
+- **Docker support** by @Taeyoung96
+- **Test data** is available, allowing evaluation of localization error and processing time.
+- **ROS2 implementation example**
+</div></details>
+
+## 📣 News
+The latest implementation demonstrates faster processing time than our paper. 
+Performance on our test data:
+- Hierarchical voxelmap construction
+  - Paper: 9,272 ms on average
+  - **Latest**: 3,494 ms on average
+  - **Load saved voxelmaps directly**: 130 ms on average (See [step 5 on this page](./test/test_code.md) to save voxelmaps)
+- Global localization
+  - Paper: 878 ms on average
+  - **Latest**: **189 ms** on average  
+
+The hardware configuration (Intel Core i7-10700K 3.8GHz, 32GB RAM, and NVIDIA GeForce RTX2060) is the same as in the paper.
+
+## ✅ Dependencies
+- bbs3d (Lower versions are not tested)
+  - CMake
+  - Eigen3 (3.4.0 or higher)
+  - OpenMP
+  - CUDA (12.0 or higher)
+- test
+  - (All bbs3d dependencies)
+  - PCL
+- ros2_test
+  - (All bbs3d dependencies)
+  - ros2 humble
+
+### Support Docker 🐳 
+
+If nvidia driver is 525.60.11 or higher, try docker!  
+For more information, you can check [docker_start.md](./docker/docker_start.md)  
+
+## 🌟 3D-BBS core source code
+### Build and Install
+```shell script
+# Note: If you are using Eigen3 below 3.4.0, git clone with --recursive
+git clone https://github.com/KOKIAOKI/3d_bbs.git
+cd 3d_bbs
+mkdir build && cd build
+```
+
+- CPU ver. & GPU ver. (Please ignore the large number of warnings)
+```shell script
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+sudo make install
+```
+
+- CPU ver. only
+```shell script
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_CUDA=OFF
+make -j
+sudo make install
+```
+
+## 📋 Test code
+See [test_code.md](./test/test_code.md)  
+<img alt="overview" src="figs/test_topreadme.png" width="40%">
+
+## 🤖 ROS 2 test code
+See [ros2_test_code.md](./ros2_test/ros2_test_code.md)  
+<img alt="overview" src="figs/rviz2_test_topreadme.png" width="40%">
+
+
+## 💻 Use 3D-BBS in your cmake project
+<details><summary> Click here! </summary><div>
+
+1. Copy `test/cmake` to your project directory
+1. Copy description above `# Common include directories` in `test/CMakeLists.txt` to `your CMakeLists.txt`
+1. Add either of the following depending on your implementation  
+If you use the CPU version, replace `gpu` with `cpu`.
+```
+find_package(Eigen3 REQUIRED)
+target_include_directories(yours ${EIGEN3_INCLUDE_DIR} ${gpu_bbs3d_LIBRARY})
+```
+</div></details>
+
+## 🌈 Acknowledgement
+I sincerely appreciate the authors for generously sharing their public code.  
+[hdl_global_localization](https://github.com/koide3/hdl_global_localization)  
+[cartographer](https://github.com/cartographer-project/cartographer)  
+[TEASER-plusplus](https://github.com/MIT-SPARK/TEASER-plusplus)   
---
+++ src/3d_bbs/test/src/gpu_test.cpp
@@ -0,0 +1,150 @@
+#include <gpu_bbs3d/bbs3d.cuh>
+#include <pointcloud_iof/pcl_eigen_converter.hpp>
+#include <pointcloud_iof/pcd_loader.hpp>
+
+#include <test.hpp>
+#include <load_yaml.hpp>
+#include <chrono>
+
+#include <pcl/common/transforms.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/gicp.h>
+
+std::string create_date() {
+  time_t t = time(nullptr);
+  const tm* local_time = localtime(&t);
+  std::stringstream s;
+  s << "20" << local_time->tm_year - 100 << "_";
+  s << local_time->tm_mon + 1 << "_";
+  s << local_time->tm_mday << "_";
+  s << local_time->tm_hour << "_";
+  s << local_time->tm_min << "_";
+  s << local_time->tm_sec;
+  return (s.str());
+}
+
+BBS3DTest::BBS3DTest() {}
+
+BBS3DTest::~BBS3DTest() {}
+
+int BBS3DTest::run(std::string config) {
+  // Load config file
+  if (!load_config(config)) {
+    std::cout << "[ERROR]  Loading config file failed" << std::endl;
+    return 1;
+  };
+
+  // Load target pcds
+  std::cout << "[Setting] Loading target pcds..." << std::endl;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr tar_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>());
+  if (!pciof::load_tar_clouds(tar_path, tar_leaf_size, tar_cloud_ptr)) {
+    std::cout << "[ERROR] Loading target pcd failed" << std::endl;
+    return 1;
+  }
+
+  std::unique_ptr<pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>> gicp_ptr;
+  if (use_gicp) {
+    gicp_ptr = std::make_unique<pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>>();
+    gicp_ptr->setInputTarget(tar_cloud_ptr);
+  };
+
+  // pcl to eigen
+  std::vector<Eigen::Vector3f> tar_points;
+  pciof::pcl_to_eigen(tar_cloud_ptr, tar_points);
+
+  // Load source pcds
+  std::cout << "[Setting] Loading source pcds..." << std::endl;
+  std::vector<std::pair<std::string, pcl::PointCloud<pcl::PointXYZ>::Ptr>> src_cloud_set;
+  if (!pciof::load_src_points_with_filename(src_path, min_scan_range, max_scan_range, src_leaf_size, src_cloud_set)) {
+    std::cout << "[ERROR] Loading source pcds failed" << std::endl;
+    return 1;
+  };
+
+  // Create output folder with date
+  std::cout << "[Setting] Create output folder with date..." << std::endl;
+  std::string date = create_date();
+  std::string pcd_save_folder_path = output_path + "/" + date;
+  boost::filesystem::create_directory(pcd_save_folder_path);
+
+  // ====3D-BBS====
+  std::unique_ptr<gpu::BBS3D> bbs3d_ptr = std::make_unique<gpu::BBS3D>();
+
+  // Set target points
+  std::cout << "[Voxel map] Creating hierarchical voxel map..." << std::endl;
+  auto initi_t1 = std::chrono::high_resolution_clock::now();
+  if (bbs3d_ptr->set_voxelmaps_coords(tar_path)) {
+    std::cout << "[Voxel map] Loaded voxelmaps coords directly" << std::endl;
+  } else {
+    bbs3d_ptr->set_tar_points(tar_points, min_level_res, max_level);
+    bbs3d_ptr->set_trans_search_range(tar_points);
+  }
+  auto init_t2 = std::chrono::high_resolution_clock::now();
+  double init_time = std::chrono::duration_cast<std::chrono::nanoseconds>(init_t2 - initi_t1).count() / 1e6;
+  std::cout << "[Voxel map] Execution time: " << init_time << "[msec] " << std::endl;
+
+  bbs3d_ptr->set_angular_search_range(min_rpy.cast<float>(), max_rpy.cast<float>());
+  bbs3d_ptr->set_score_threshold_percentage(static_cast<float>(score_threshold_percentage));
+  if (timeout_msec > 0) {
+    bbs3d_ptr->enable_timeout();
+    bbs3d_ptr->set_timeout_duration_in_msec(timeout_msec);
+  }
+
+  // localization
+  double sum_time = 0;
+  int num_localized = 0;
+  for (const auto& src_cloud : src_cloud_set) {
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "[Localize] pcd file name: " << src_cloud.first << std::endl;
+
+    std::vector<Eigen::Vector3f> src_points;
+    pciof::pcl_to_eigen(src_cloud.second, src_points);
+    bbs3d_ptr->set_src_points(src_points);
+    bbs3d_ptr->localize();
+
+    std::cout << "[Localize] Execution time: " << bbs3d_ptr->get_elapsed_time() << "[msec] " << std::endl;
+    std::cout << "[Localize] Score: " << bbs3d_ptr->get_best_score() << std::endl;
+
+    if (!bbs3d_ptr->has_localized()) {
+      if (bbs3d_ptr->has_timed_out())
+        std::cout << "[Failed] Localization timed out." << std::endl;
+      else
+        std::cout << "[Failed] Score is below the threshold." << std::endl;
+      continue;
+    }
+
+    sum_time += bbs3d_ptr->get_elapsed_time();
+    num_localized++;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>());
+    if (use_gicp) {
+      gicp_ptr->setInputSource(src_cloud.second);
+      gicp_ptr->align(*output_cloud_ptr, bbs3d_ptr->get_global_pose());
+
+      Eigen::Isometry3f estimated_pose;
+      estimated_pose.matrix() = bbs3d_ptr->get_global_pose();
+      Eigen::Isometry3f ground_truth_pose;
+      ground_truth_pose.matrix() = gicp_ptr->final_transformation_;
+
+      Eigen::Isometry3f error = estimated_pose.inverse() * ground_truth_pose;
+      float error_t = error.translation().norm();
+      float error_r = Eigen::AngleAxisf(error.linear()).angle();
+      std::cout << "[Localize] Translation error: " << error_t << ", Rotation error: " << error_r << std::endl;
+    } else {
+      pcl::transformPointCloud(*src_cloud.second, *output_cloud_ptr, bbs3d_ptr->get_global_pose());
+    }
+    pcl::io::savePCDFileBinary(pcd_save_folder_path + "/" + src_cloud.first + ".pcd", *output_cloud_ptr);
+  }
+
+  if (num_localized != 0) {
+    std::cout << "[Localize] Average time: " << sum_time / num_localized << "[msec] per frame" << std::endl;
+  }
+
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  std::string config = argv[1];
+  BBS3DTest test;
+  return test.run(config);
+}
