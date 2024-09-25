ac724633e7623dfceb0f3014ee1ba216769fc2f2
Pesrine <482566032@qq.com>
2024-09-26 00:28:57 +0800

    new

---
+++ src/3d_bbs/build/CMakeFiles/cpu_bbs3d.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpu_bbs3d.dir/bbs3d/src/cpu_bbs3d/bbs3d.cpp.o"
+  "CMakeFiles/cpu_bbs3d.dir/bbs3d/src/cpu_bbs3d/bbs3d.cpp.o.d"
+  "CMakeFiles/cpu_bbs3d.dir/bbs3d/src/cpu_bbs3d/voxelmaps.cpp.o"
+  "CMakeFiles/cpu_bbs3d.dir/bbs3d/src/cpu_bbs3d/voxelmaps.cpp.o.d"
+  "CMakeFiles/cpu_bbs3d.dir/bbs3d/src/cpu_bbs3d/voxelmaps_io.cpp.o"
+  "CMakeFiles/cpu_bbs3d.dir/bbs3d/src/cpu_bbs3d/voxelmaps_io.cpp.o.d"
+  "libcpu_bbs3d.pdb"
+  "libcpu_bbs3d.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpu_bbs3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/3d_bbs/build/CMakeFiles/cpu_bbs3d.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cpu_bbs3d.
+# This may be replaced when dependencies are built.
